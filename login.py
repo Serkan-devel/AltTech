7cb486dd3b682f97afc07ea8b8f93edb5fce4ebb
UndeadMockingbird <frankriese@MacBook-Pro.fritz.box>
2018-08-15 02:56:58 +0200

    Transformed Minds code into class

--- login.py
+++ login.py
@@ -1,26 +1,50 @@
 import sys
 import requests
+import json
 
-loginUrl = 'https://www.minds.com/api/v1/authenticate'
+class MindsAPI:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-client = requests.session()
-client.get(loginUrl)
+    def login(self):
+        loginUrl = 'https://www.minds.com/api/v1/authenticate'
+        self.client = requests.session()
+        self.client.get(loginUrl)
+        headers = {
+            'cookie': 'loggedin=1; minds='+ self.client.cookies['minds'] +'; XSRF-TOKEN='+ self.client.cookies['XSRF-TOKEN'],
+            'x-xsrf-token': self.client.cookies['XSRF-TOKEN']
+        }
+        loginData = {
+            'username' : self.username,
+            'password' : self.password,
+            'x-xsrf-token' : self.client.cookies['XSRF-TOKEN']
+        }
+        r = self.client.post(loginUrl, data = loginData, headers = headers)
+        #print(r.content)
 
-headers = {
-    'cookie': 'loggedin=1; minds='+ client.cookies['minds'] +'; XSRF-TOKEN='+ client.cookies['XSRF-TOKEN'],
-    'x-xsrf-token': client.cookies['XSRF-TOKEN']
-}
-loginData = {
-    'username' : 'zippypippytippy',
-    'password' : 'Obama08*',
-    'x-xsrf-token' : client.cookies['XSRF-TOKEN']
-}
-r = client.post(loginUrl, data = loginData, headers = headers)
-print(r.content)
+        self.headers = {
+            'cookie': 'loggedin=1; minds='+ self.client.cookies['minds'] +'; XSRF-TOKEN='+ self.client.cookies['XSRF-TOKEN'],
+            'x-xsrf-token': self.client.cookies['XSRF-TOKEN']
+        }
 
-headers = {
-    'cookie': 'loggedin=1; minds='+ client.cookies['minds'] +'; XSRF-TOKEN='+ client.cookies['XSRF-TOKEN'],
-    'x-xsrf-token': client.cookies['XSRF-TOKEN']
-}
-r = client.get('https://www.minds.com/api/v1/notifications/all', headers = headers)
-print(r.content)
+    def GetNotifications(self):
+        r = self.client.get('https://www.minds.com/api/v1/notifications/all', headers = self.headers)
+        return json.loads(r.content)
+
+    def GetChannel(self, channelName):
+        r = self.client.get('https://www.minds.com/api/v1/channel/'+ channelName, headers = self.headers)
+        return json.loads(r.content)
+
+def JsonPrint(js):
+    print(json.dumps(js, indent=4, sort_keys=True))
+
+minds = MindsAPI('zippypippytippy','Obama08*')
+minds.login()
+
+#j = minds.GetNotifications()
+#JsonPrint(j)
+
+j = minds.GetChannel('UndeadMockingbird')
+print('View count:', j['channel']['impressions'])
+print('Subscribers:', j['channel']['subscribers_count'])
