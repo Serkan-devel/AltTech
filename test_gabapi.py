5cf505b1d0a1937a725b758960b64b8c52192d6c
Undead Mockingbird <undeadmockingbird@protonmail.com>
2018-09-17 22:07:43 +0200

    test script

--- test_gabapi.py
+++ test_gabapi.py
@@ -9,14 +9,13 @@ password = os.environ['GAB_PW']
 
 api = gabapi.GabAPI(username, password)
 api.login()
-r = api.get_notifications()
+api.post_comment('<p style="color:red">html test!</p>')
+#api.post_comment('<font color="red">rrrrrr</font>','img.png')
+quit()
 
+r = api.get_notifications()
 for n in r.json()['data']:
     if n['type'] == 'comment':
         print("{}: {}".format(n['actuser']['name'], n['post']['body']))
     else:
         print(n['message'])
-
-
-#r = api.post_comment('TTT','img.png')
-#print(r)
